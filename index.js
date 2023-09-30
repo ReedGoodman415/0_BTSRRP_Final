a57da2fa41933467a6e2a2c780f4027af11e23ce
Reed <reed@goodmandesign.net>
2023-09-30 13:26:45 -0700

    Adds a map of the San Francisco Bay Area to the about page. (generated by useblackbox.ai )

--- index.js
+++ index.js
@@ -10222,6 +10222,27 @@ a.text-dark:hover, a.text-dark:focus {
   }
 }
 javascript
+function initMap() {
+ var bayArea = {lat: 37.7749, lng: -122.4194};
+ var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: bayArea
+ });
+
+ var marker = new google.maps.Marker({
+    position: bayArea,
+    map: map
+ });
+}
+javascript
+var map = new google.maps.Map(document.getElementById('map'), {
+ zoom: 12,
+ center: bayArea,
+ styles: [
+    {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+    {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+    {elementType: '
+javascript
 javascript
 var map = new google.maps.Map(document.getElementById('map'), {
  zoom: 12,
