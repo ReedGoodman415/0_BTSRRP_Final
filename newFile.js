05e46996cfd9702d99b996d326ab8e50067ae7b3
Reed <reed@goodmandesign.net>
2023-09-30 13:38:29 -0700

    Update .hintrc, index.html, and newFile.js (generated by useblackbox.ai )

--- newFile.js
+++ newFile.js
@@ -83,6 +83,18 @@
     subClass.prototype.constructor = subClass;
     subClass.__proto__ = superClass;
   }
+  var map = new ol.Map({
+    target: 'map',
+    layers: [
+       new ol.layer.Tile({
+         source: new ol.source.OSM()
+       })
+    ],
+    view: new ol.View({
+       center: ol.proj.fromLonLat([-122.4194, 37.7749]),
+       zoom: 12
+    })
+   });
 
   /**
    * --------------------------------------------------------------------------
